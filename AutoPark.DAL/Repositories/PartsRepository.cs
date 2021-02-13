d45de4abf020cb86582140a37a87450c7cd1a922
Dmitry <dmitri.bohdan@gmai.com>
2021-02-13 19:27:17 +0300

    added PartsRepository

---
+++ AutoPark.DAL/Repositories/PartsRepository.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoPark.DAL.Entities;
+using AutoPark.DAL.Interfaces;
+using Dapper;
+
+namespace AutoPark.DAL.Repositories
+{
+    public class PartsRepository : ConnectionRepository, IRepository<Part>
+    {
+        protected PartsRepository(string connectionString) : base(connectionString)
+        {
+        }
+
+        public async Task<IEnumerable<Part>> GetAll()
+        {
+            await using (connection)
+            {
+               return await connection.QueryAsync<Part>("select * from parts");
+            }
+        }
+
+        public async Task<Part> Get(int id)
+        {
+            await using (connection)
+            {
+                return await connection.QueryFirstAsync<Part>("select * from parts where id = @Id}", new {id});
+            }
+        }
+
+        public async Task Create(Part part)
+        {
+            await using (connection)
+            {
+                await connection.ExecuteAsync("insert into parts(part_name) values(@Part_Name)", part);
+            }
+        }
+
+        public async Task Update(Part part)
+        {
+            await using (connection)
+            {
+                await connection.ExecuteAsync("update parts set part_name = @Part_Name where id = @Id", part);
+            }
+        }
+
+        public async Task Delete(int id)
+        {
+            await using (connection)
+            {
+                await connection.ExecuteAsync("delete from parts where id = @id", new {id});
+            }
+        }
+    }
+}
